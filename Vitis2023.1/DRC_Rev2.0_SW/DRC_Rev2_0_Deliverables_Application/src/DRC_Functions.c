53236903ac6e3d767aa27797449df15f3d6468b5
Yousef Khalil <yousefkhalil0311@gmail.com>
2025-04-15 17:30:04 -0400

    Software DAC Controller completed. Test Mode 8 can test DACs in normal and quadrature mode (real mixing and complex mixing). Quadrature mode DAC frequency adjustable with accuracy of 0.0008%. Amplitutde control works. Will add color output to terminal and then merge with the main branch.

--- Vitis2023.1/DRC_Rev2.0_SW/DRC_Rev2_0_Deliverables_Application/src/DRC_Functions.c
+++ Vitis2023.1/DRC_Rev2.0_SW/DRC_Rev2_0_Deliverables_Application/src/DRC_Functions.c
@@ -4,6 +4,7 @@
 #include "XGpio.h"
 #include "DRC_Parameters.h"
 #include "IOEXP.h"
+#include "AFE7222.h"
 
 //sets the Status of the edge mounted LEDs.
 void setLEDStatus (uint8_t status){
@@ -98,4 +99,48 @@ void displayPinState(uint8_t pinNum, uint8_t state){
 	printf("\033[%d;%dH", 4 + pinNum%30, 30 * (pinNum/30));
 	printf("Pin %d: %d\n", pinNum, state);
 	return;
+}
+
+//Programs all AFE converters with the values in the regMap register map
+void programAFEConverters(uint8_t* regMap, int regMapSize){
+	for (int i = 0x01; i < (0x01 << 4); i = i << 1){
+		   int Status = AFE_Init(&SPI0_AFE, regMap, regMapSize, i);
+		   if(Status != XST_SUCCESS) return XST_FAILURE;
+	}
+}
+
+//Gets a register value from the regMap and register address specified
+uint8_t getRegMapVal(uint8_t* regMap, int regMapSize, uint16_t regNum){
+	for(int regIndex = 0; regIndex < regMapSize; regIndex+=3){
+
+		//store first 12 bits of each 3 byte sequence as register address
+		uint16_t currentReg = (regMap[regIndex] << 4) | (regMap[regIndex + 1] >> 4);
+
+		//return current register content of regNum in regMap (bits 4-11)
+		if(regNum == currentReg){
+			return (regMap[regIndex + 1] << 4) | (regMap[regIndex + 2] >> 4);
+		}
+	}
+
+	printf("Error: getRegMapVal - Failed to find register. ");
+	return 0;
+}
+
+//sets a value in the regMap and register address specified
+void setRegMapVal(uint8_t* regMap, int regMapSize, uint16_t regNum, uint8_t regVal){
+
+	for(int regIndex = 0; regIndex < regMapSize; regIndex+=3){
+
+		//store first 12 bits of each 3 byte sequence as register address
+		uint16_t currentReg = (regMap[regIndex] << 4) | (regMap[regIndex + 1] >> 4);
+
+		//return current register content of regNum in regMap (bits 4-11)
+		if(regNum == currentReg){
+			regMap[regIndex + 1] = (regMap[regIndex + 1] & 0xF0) | (regVal >> 4);
+			regMap[regIndex + 2] = (regVal << 4) & 0xF0;
+			return;
+		}
+	}
+
+	printf("Error: setRegMapVal - Failed to find register. ");
 }
